1fdd9d768f971fe99fc6411b1c14bc00eaf7e542
gangadhar0709 <jagarapugangadhar@gmail.com>
2026-07-03 06:11:56 +0530

    Implemented Milestone 1: User registration and Spring Security setup

---
+++ src/main/java/com/example/backend/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.example.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.backend.dto.RegisterRequest;
+import com.example.backend.service.UserService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+
+        String response = userService.registerUser(request);
+
+        return ResponseEntity.ok(response);
+    }
+}
