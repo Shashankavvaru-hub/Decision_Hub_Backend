6460c40e3757482e1f44131a0e2ab31358c285b4
gangadhar0709 <jagarapugangadhar@gmail.com>
2026-07-04 18:21:10 +0530

    Implemented JWT token generation and login authentication

--- src/main/java/com/example/backend/controller/AuthController.java
+++ src/main/java/com/example/backend/controller/AuthController.java
@@ -1,5 +1,8 @@
 package com.example.backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import com.example.backend.dto.AuthResponse;
+import com.example.backend.dto.LoginRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -24,6 +28,14 @@ public class AuthController {
     public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
 
         String response = userService.registerUser(request);
+
+        return ResponseEntity.ok(response);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody LoginRequest request) {
+
+        AuthResponse response = userService.loginUser(request);
 
         return ResponseEntity.ok(response);
     }
--- src/main/java/com/example/backend/service/UserService.java
+++ src/main/java/com/example/backend/service/UserService.java
@@ -5,21 +5,28 @@ import java.time.LocalDateTime;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.backend.dto.AuthResponse;
+import com.example.backend.dto.LoginRequest;
 import com.example.backend.dto.RegisterRequest;
 import com.example.backend.entity.Role;
 import com.example.backend.entity.User;
 import com.example.backend.repository.UserRepository;
+import com.example.backend.security.JwtService;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public UserService(UserRepository userRepository,
-                       PasswordEncoder passwordEncoder) {
+                       PasswordEncoder passwordEncoder,
+                       JwtService jwtService) {
+
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
     }
 
     public String registerUser(RegisterRequest request) {
@@ -39,5 +46,25 @@ public class UserService {
         userRepository.save(user);
 
         return "User Registered Successfully!";
+    }
+
+    public AuthResponse loginUser(LoginRequest request) {
+
+        User user = userRepository
+                .findByEmail(request.getEmail())
+                .orElseThrow(
+                        () -> new RuntimeException("User not found!")
+                );
+
+        if (!passwordEncoder.matches(
+                request.getPassword(),
+                user.getPassword())) {
+
+            throw new RuntimeException("Invalid password!");
+        }
+
+        String token = jwtService.generateToken(user.getEmail());
+
+        return new AuthResponse(token);
     }
 }
